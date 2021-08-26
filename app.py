a5e923df47c3b7d2c394fa79f7150233b2ad48ff
Vishva Patel <patelvishu2607@gmail.com>
2021-08-26 16:50:04 +0530

    add new files 7

--- app.py
+++ app.py
@@ -195,6 +195,10 @@ def create_app():
         return jsonify({'message': 'Hello,hello, World!'})
 
 
+
+app = create_app()
+
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT",5000))
     app.run(host='127.0.0.1',port=port,debug = True)
